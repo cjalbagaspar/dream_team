914b65af456aede816a9049bef840dffcd58d148
ronneldarylrontos <ronneldarylrontos@gmail.com>
2023-05-16 16:19:16 +0800

    Authentication

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 if(isset($_POST['submit'])){
 
     $name = $_POST['name'];
+    $password = $_POST['password'];
 
-    echo 'HELLO' .$name;
+    if($name === 'ron' && $password === '123'){
+     
+        header('Location: main.php');
+        die();
+    }else{
+ 
+        echo 'LOGIN FAILED';
+
+        
+    }
 }
 
 ?>
@@ -19,6 +29,10 @@ Username: <input name="name" type="text"><br>
 Password: <input name="password" type="password"><br>
 <input type="submit" name="submit">
 </form>
+
+
+
+
 
 </body>
 </html>
