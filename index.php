200ddb3264887f1e3711293e1cdb37a3b0690742
ronneldarylrontos <ronneldarylrontos@gmail.com>
2023-05-16 16:43:20 +0800

    session added

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
+session_start();
 
-if(isset($_POST['submit'])){
-
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $password = $_POST['password'];
 
-    if($name === 'ron' && $password === '123'){
-     
+    if ($name === 'ron' && $password === '123') {
+        $_SESSION['username'] = $name;
         header('Location: main.php');
-        die();
-    }else{
- 
-        echo 'LOGIN FAILED';
-
-        
+        exit();
+    } else {
+        $error = 'LOGIN FAILED';
     }
 }
-
 ?>
 
 <html>
@@ -24,15 +20,15 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 
+<?php if (isset($error)) { ?>
+    <p><?php echo $error; ?></p>
+<?php } ?>
+
 <form action="" method="post">
-Username: <input name="name" type="text"><br>
-Password: <input name="password" type="password"><br>
-<input type="submit" name="submit">
+    Username: <input name="name" type="text"><br>
+    Password: <input name="password" type="password"><br>
+    <input type="submit" name="submit">
 </form>
-
-
-
-
 
 </body>
 </html>
