2cec7ec8cddcb523042c84d5fedced2047639d31
Carlo <cjalbagaspar@gmail.com>
2023-05-16 14:33:40 +0800

    create cloing form

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 
-
+<form action="" method="post">
 Username: <input name="username" type="text"><br>
 Password: <input name="password" type="password"><br>
 <button type="submit">Login</button>
+</form>
+
