f07e6547eac58d3555f6e5fe244049aab90cf9b0
ronneldarylrontos <ronneldarylrontos@gmail.com>
2023-05-18 09:59:45 +0800

    added error msg

--- index.php
+++ index.php
@@ -2,35 +2,44 @@
 session_start(); 
 
 
-
+$msg = "";
 //this condition checked if username is correct
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $password = $_POST['password'];
 
-    if ($name === 'ron' && $password === '123') {
+    if ($name === 'admin' && $password === 'admin') {
         $_SESSION['username'] = $name;
         header('Location: main.php');
         exit();
     } else {
         if ($name !== 'ron') {
-            echo 'Incorrect username';
+            $msg =  'Incorrect username';
         } elseif ($password !== '123') {
-            echo 'Incorrect password';
+            $msg =  'Incorrect password';
         }
     }
 }
-?>
+?> 
+
+
 
 <html>
 <head>
+<style type="text/css">
+.error{
+	color:red;
+	font-weight:bold;
+}
+</style>
 </head>
 <body>
 
 
 
 <form action="" method="post">
+	<div class="<?php if($msg) echo "error"; ?>"><?php echo $msg; ?></div>
     Username: <input name="name" type="text"><br>
     Password: <input name="password" type="password"><br>
     <input type="submit" name="submit">
