00462c7faa1aac2c5069e51fd31519ca27950098
unknown <markkevincosme009@gmail.com>
2023-05-19 09:53:02 +0800

    adding css code

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
-<?php
+<html>
+
+<head>
+
+    <style>
+    .button {
+        background-color: black;
+        /* Green */
+        border: none;
+        color: white;
+        padding: 10px 10px;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+    }
+/* Padding background */
+    .pad {
+        border: 6px solid black;
+        background-color: red;
+        padding-top: 50px;
+        padding-right: 50px;
+        padding-bottom: 50px;
+        padding-left: 50px;
+        text-align: center;
+        width: 300px;
+        height: 100px;
+        
+
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="pad">
+        <form action="" method="post">
+            Username: <input name="name" type="text"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" class="button" name="submit">
+        </form>
+    </div>
+
+    <?php
+
 
 session_start(); 
 
