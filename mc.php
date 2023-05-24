0c7b7a8888802ce3ed2484abaaf6b6e156728833
Jaypeejapss <pauldoctolero0716@gmail.com>
2023-05-24 16:33:51 +0800

    adding authenticator login

--- mc.php
+++ mc.php
@@ -1,6 +1,84 @@
-a
-b
-c hakdog hakdog
-d doctolero
-e ellapante
-f fishcracker
+<?php
+session_start(); 
+
+
+
+
+
+if(isset($_GET["logout"])){
+	session_destroy();
+	die(123);
+}
+ 
+if(isset($_SESSION['username'])){
+	
+	
+	header('Location: main.php');
+	die();
+}
+
+
+$msg = "";
+
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    if ($name === 'ron' && $password === '123') {
+        $_SESSION['username'] = $name;
+        header('Location: main.php');
+        exit();
+    } else {
+
+
+        if ($name !== 'ron') {
+            echo 'Incorrect username';
+        } elseif ($password !== '123') {
+            echo 'Incorrect password';
+
+
+
+        if ($name !== 'admin') {
+            $msg =  'Incorrect username';
+        } elseif ($password !== 'admin') {
+            $msg =  'Incorrect password';
+
+
+        }
+    }
+}
+}
+?>
+
+<html>
+
+<head>
+
+<style type="text/css">
+.error{
+	color:red;
+	font-weight:bold;
+}
+
+</style>
+</head>
+<body>
+
+<h1>ADDITION</h1>
+
+<h2>ENTER YOUR CREDENTIALS</h2>
+
+
+
+
+<div class="pad">
+        <form action="" method="post">
+            Username: <input name="name" type="text"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" class="button" name="submit">
+        </form>
+    </div>
+
+</body>
+</html>
