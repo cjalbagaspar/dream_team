68157d206f62aa76e4a268fcf550c3fbfc29f723
unknown <markkevincosme009@gmail.com>
2023-05-24 16:20:06 +0800

    adding new features and design

--- index.php
+++ index.php
@@ -71,6 +71,30 @@ if (isset($_POST['submit'])) {
 
 
 }
+.button {
+        background-color: black;
+        /* Green */
+        border: none;
+        color: white;
+        padding: 10px 10px;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+    }
+/* Padding background */
+    .pad {
+        border: 6px solid black;
+        background-color: red;
+        padding-top: 50px;
+        padding-right: 50px;z
+        padding-bottom: 50px;
+        padding-left: 50px;
+        text-align: center;
+        width: 300px;
+        height: 100px;
+        
+
+    }
 </style>
 </head>
 <body>
